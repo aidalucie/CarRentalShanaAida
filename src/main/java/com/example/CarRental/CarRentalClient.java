27afdf3e83214d574fbfc46392a23e91cdd1dce4
aidalucie <ndourlucie@gmail.com>
2019-11-15 15:12:34 +0100

    Initial commit

---
+++ src/main/java/com/example/CarRental/CarRentalClient.java
@@ -0,0 +1,28 @@
+package com.example.CarRental;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class CarRentalClient {
+
+    private static final Logger log = LoggerFactory.getLogger(CarRentalClient.class);
+
+    public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<Car> request = new HttpEntity<>(new Car("44QQ66", "Peugeot", 12));
+        restTemplate.postForObject("http://localhost:8080/cars", request, Car.class);
+
+
+
+        List cars = restTemplate.getForObject("http://localhost:8080/cars", List.class);
+        log.info(cars.toString());
+
+    }
+
+}
---
+++ src/main/java/com/example/CarRental/CarRentalController.java
@@ -0,0 +1,4 @@
+package com.example.CarRental;
+
+public class CarRentalController {
+}
